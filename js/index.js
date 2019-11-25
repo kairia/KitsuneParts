7a62ab26d0f4fbbfeeccc5e158ff8bef77e45fce
kairia <8695024+kairia@users.noreply.github.com>
2019-11-26 02:50:31 +0800

    startup

---
+++ js/index.js
@@ -0,0 +1,7 @@
+window.addEventListener('load',()=>{
+
+    const preload=document.querySelector('.preload');
+    const nav=document.querySelector('.nav');
+    preload.classList.add('preload-finish');
+    nav.classList.add('nav-engage');
+    });
