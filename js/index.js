eb5c9c202f46f1631f7376bcb76c13869d63693a
kairia <8695024+kairia@users.noreply.github.com>
2019-11-27 02:09:46 +0800

    update1127

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-
+var block = document.querySelectorAll('.block');
+var Animstop = new Boolean(false);
 /* preloader */
 $('html, body').css({
     'overflow': 'hidden',
@@ -11,12 +12,34 @@ window.addEventListener('load', () => {
     $('html, body').removeAttr('style')
 });
 /* preloader */
-const tween = new TimeLineLite();
+/* randomanimater */
+function setProperty(height,width,horizon,vertic) {
+    const selected = Math.round(Math.random()*15);
+    block[selected].style.setProperty('--randomHeight', height +'px' );
+    block[selected].style.setProperty('--randomWidth', width +'px');
+    block[selected].style.setProperty('--randomX', horizon );
+    block[selected].style.setProperty('--randomY', vertic );
+  }
+  function changePercentage() {
+    var height = Math.random()*100;
+    var width = Math.random()*100;
+    var horizon = Math.random()*3000+100;
+    var vertic = Math.random()*1000;
+    setProperty(height,width,horizon,vertic);
+  }
+  setInterval(changePercentage, 60);
+/* randomanimater */
 
 const controller = new ScrollMagic.Controller();
 const nav = new ScrollMagic.Scene({
     triggerElement: '.info',
-    duration:3000
-});
+    duration:0,
+    triggerHook:0,
+})
+  .setClassToggle(".nav", "nav-engage").reverse(true)
+  .addIndicators()
+  .addTo(controller);
+
+
 
 
